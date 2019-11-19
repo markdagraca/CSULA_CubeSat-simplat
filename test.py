0da071e4155d17b298b8309cc8d6a4251ed40e12
markdagraca <markdagraca1@gmail.com>
2019-11-18 19:33:31 -0800

    Test code for russ

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ sio = socketio.Client()
 
 sio.connect('http://192.168.43.209:5000')
 for x in range(0,100):  
-    sio.emit('move', {'x':100-x,'y':0})
+    sio.emit('thruster', {'forward':100-x,'left':x})
     time.sleep(1/10)
 
 time.sleep(5)
